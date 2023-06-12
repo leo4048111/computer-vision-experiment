3ec94d561a317e43cf22d09660107f4b5ffc841a
GodAlmightyByMySide <74029782+leo4048111@users.noreply.github.com>
2023-06-12 15:10:31 +0800

    Updated download scripts and configs
    Updated download scripts and configs

--- unsup3d/data/download_cat.sh
+++
@@ -1,2 +0,0 @@
-echo "----------------------- downloading cat dataset -----------------------"
-curl -o cat_combined.zip "https://www.robots.ox.ac.uk/~vgg/research/unsup3d/data/cat_combined.zip" && unzip cat_combined.zip
